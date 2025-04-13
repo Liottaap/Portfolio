230c02f3e79796739e25f9d2a8f93c997f5428fe
Ana Liotta <liotta.a.p@gmail.com>
2025-04-12 22:00:14 -0300

    cambio font-size en header y text aling en footer

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,7 +6,7 @@ import'../globals.css'
 function Contact() {
     const animatedTitle = useAnimatedTitle('CONTACT', 150)
     return(
-        <footer id='contact' className=" text-whiteLetter w-full flex flex-col gap-10 items-center justify-center p-10 lg:p-20">
+        <footer id='contact' className=" text-whiteLetter w-full flex flex-col gap-10 items-center justify-center p-10 lg:p-20 text-center">
             <h2 className=" text-5xl font-bold bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">{animatedTitle}</h2>
             <p className=" text-4xl  ">Trabajemos juntos!</p>
             <div className="flex flex-col  gap-5 justify-center items-center text-2xl">
